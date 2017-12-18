a9e87b84a95ced51fb479186f05ab073ecd1bd4d
Victor Irzak <victor.irzak@zomp.com>
2017-12-17 23:37:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRA.Typescript.Dotnet.ProjectTemplate
+A template for CRA, Typescript and ASP.NET Core.
+
+This project is a combination of [react-app-rewired](https://github.com/timarney/react-app-rewired) and
+[React SPA Project templates for ASP.NET Core](https://github.com/aspnet/templating/tree/dev/src/Microsoft.DotNet.Web.Spa.ProjectTemplates)
---
+++ content/React-CSharp/ClientApp/src/index.tsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <BrowserRouter basename={baseUrl}>
+    <App />
+  </BrowserRouter>,
+  rootElement);
+
+registerServiceWorker();
